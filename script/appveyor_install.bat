b80d429fedb9c55fb3ba55f7f440a7190ddb168b
Alex Robenko <arobenko@gmail.com>
2022-04-30 17:15:01 +1000

    Testing build with VS2022 in appveyor.

--- script/appveyor_install.bat
+++ script/appveyor_install.bat
@@ -26,6 +26,15 @@ IF "%APPVEYOR_BUILD_WORKER_IMAGE%"=="Visual Studio 2015" (
         echo Performing amd64 build in VS2019
         call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
     )
+) ELSE IF "%APPVEYOR_BUILD_WORKER_IMAGE%"=="Visual Studio 2022" (
+    set TOOLCHAIN=msvc17
+    IF "%PLATFORM%"=="x86" (
+        echo Performing x86 build in VS2022
+        call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars32.bat"
+    ) ELSE (
+        echo Performing amd64 build in VS2022
+        call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
+    )    
 ) ELSE (
     echo Toolchain %TOOLCHAIN% is not supported
     exit -1
